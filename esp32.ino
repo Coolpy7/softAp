32d1fbdfffba55a0d3828eb784e56a30d16e5400
黎东海 <jacle169@21cn.com>
2021-07-05 13:17:19 +0800

    init

---
+++ esp32.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+#include <WiFiUdp.h> //引用以使用UDP
+#include <ArduinoJson.h>
+
+const char *ssid = "esp32";
+const char *password = "";
+
+WiFiUDP Udp;                      //创建UDP对象
+unsigned int localUdpPort = 2333; //本地端口号
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password);
+
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  Udp.begin(localUdpPort); //启用UDP监听以接收数据
+}
+
+void loop()
+{
+  int packetSize = Udp.parsePacket(); //获取当前队首数据包长度
+  if (packetSize)                     //如果有数据可用
+  {
+    char buf[packetSize];
+    Udp.read(buf, packetSize); //读取当前包数据
+
+    Serial.print("From IP: ");
+    Serial.println(Udp.remoteIP());
+    Serial.print("From Port: ");
+    Serial.println(Udp.remotePort());
+
+    DynamicJsonDocument doc(64);
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, buf);
+    // Test if parsing succeeds.
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      return;
+    }
+    const char* rssid = doc["ssid"];
+    Serial.println(String(rssid));
+    const char* rpassword = doc["password"];
+    Serial.println(String(rpassword));
+
+    DynamicJsonDocument outDoc(64);
+    outDoc["code"] = 0;
+    String output;
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort()); //准备发送数据
+    serializeJson(outDoc, Udp);
+    Udp.println();
+    Udp.endPacket();
+  }
+}
---
+++ pages/index/index.js
@@ -0,0 +1,140 @@
+Page({
+  data: {
+      ssid: 'esp32',
+      password: '',
+      udp: '',
+      port: 2333,
+      showClearBtn: false,
+      isFirst: true,
+  },
+  onLoad(opt) {
+      let that = this
+      wx.startWifi({
+          success(res) {
+              console.log(res.errMsg, 'wifi初始化成功')
+              that.getWifiInfo();
+          },
+          fail: function (res) {
+              wx.showToast({
+                  title: '请连接路由器!',
+                  duration: 2000,
+                  icon: 'none'
+              })
+          }
+      })
+      this.getWifiInfo()
+  },
+  getWifiInfo() {
+      let that = this
+      wx.getConnectedWifi({
+          success(res) {
+              console.log("getConnectedWifi ok:", JSON.stringify(res))
+              if ('getConnectedWifi:ok' === res.errMsg) {
+                  that.setData({
+                      ssid: res.wifi.SSID,
+                      bssid: res.wifi.BSSID
+                  })
+              } else {
+                  wx.showToast({
+                      title: '请连接路由器',
+                      duration: 2000,
+                      icon: 'none'
+                  })
+              }
+          },
+          fail(res) {
+              wx.showToast({
+                  title: '请连接路由器',
+                  duration: 2000,
+                  icon: 'none'
+              })
+          }
+      })
+  },
+  onInputSSID(evt) {
+      const {
+          value
+      } = evt.detail;
+      this.setData({
+          ssid: value,
+      });
+  },
+  onInputPassword(evt) {
+      const {
+          value
+      } = evt.detail;
+      this.setData({
+          password: value,
+          showClearBtn: !!value.length,
+          isWaring: false,
+      });
+  },
+  onClear() {
+      this.setData({
+          password: '',
+          showClearBtn: false,
+          isWaring: false,
+      });
+  },
+  onConfirm() {
+      console.log("ssid:", this.data.ssid, ",password:", this.data.password)
+      if (this.data.isFirst) {
+          let udp = wx.createUDPSocket();
+          this.setData({
+              isFirst: false,
+              udp,
+              port: udp.bind(),
+          });
+      }
+
+      const password = this.data.password;
+      const ssid = this.data.ssid;
+      const port = this.data.port;
+
+      let message = JSON.stringify({
+          port,
+          password,
+          ssid
+      })
+
+      this.data.udp.send({
+          address: '192.168.4.1',
+          port: 2333,
+          message
+      });
+
+      this.data.udp.onMessage((res) => {
+          //字符串转换，很重要
+          let unit8Arr = new Uint8Array(res.message);
+          let encodedString = String.fromCharCode.apply(null, unit8Arr);
+          console.log("data:", encodedString);
+          let str = JSON.parse(encodedString);
+          switch (str.code) {
+              //成功收到信息
+              case 0:
+                  wx.showToast({
+                      title: '接收成功',
+                  })
+                  break;
+              //成功解析到信息
+              case 1:
+                  wx.showToast({
+                      title: '准备连接路由器',
+                  })
+                  break;
+              //成功连接到路由器
+              case 2:
+                  wx.showToast({
+                      title: '成功连接',
+                  })
+                  break;
+              //连接失败路由器
+              case 3:
+                  wx.showToast({
+                      title: '连接失败',
+                  })
+                  break;
+          }
+      })
+  },
+});
